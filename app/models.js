4d55a58f6834f39cd56acdd52e8c46970e3eae2f
Cunningham, Nick <Nick_Cunningham@intuit.com>
2015-10-10 00:02:41 -0700

    create initial data models

--- app/models.js
+++ app/models.js
@@ -12,6 +12,43 @@ var Contact = new Schema({
     gravatar:   { type: String }
 });
 
+var GameEntry = new Schema({
+    match : {
+        stock : {type: Number, default: 3},
+        timer : {type: Number, default: 0},
+        items : {type: Boolean, default: false},
+        players : {type: Number, default: 2},
+        customs : {type: Boolean, default: false)},
+        date : {
+            fought : {type: Date, default: Date.now},
+            recorded : {type: Date, default: Date.now}
+        }
+    },
+    players : [{
+        id : String,
+        kills : Number,
+        deaths : Number,
+        suicides : Number,
+        character : String,
+        rank : Number,
+        controllerType : String
+    }]
+});
+
+var Player = new Schema ({
+    id : String,
+    name : String,
+    gravatar : String,
+    description : String
+});
+
+var Character = new Schema ({
+    id : String,
+    name : String
+});
+
 module.exports = {
-    Contact: mongoose.model('Contact', Contact)
+    Contact: mongoose.model('GameEntry', GameEntry),
+    Contact: mongoose.model('Player', Player),
+    Contact: mongoose.model('Character', Character)
 };
